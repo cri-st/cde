1a07baaa6e5ac99c535df0eaf8a94d5590cb916a
Crist Gimenez <mail@cri.st>
2022-10-10 18:53:40 -0300

    Add Thymeleaf Templates

--- src/main/java/st/cri/cde/controller/PayrollController.java
+++ src/main/java/st/cri/cde/controller/PayrollController.java
@@ -1,20 +1,48 @@
 package st.cri.cde.controller;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import st.cri.cde.dto.ScribeDto;
 import st.cri.cde.service.ScribesCollegeService;
 
-@RestController
+@Slf4j
+@Controller(value = "/")
 @AllArgsConstructor
 public class PayrollController {
 
     private ScribesCollegeService scribesCollegeService;
 
+    @GetMapping
+    public String query(Model model) {
+        final ScribeDto scribe = new ScribeDto();
+        model.addAttribute("scribe", scribe);
+        return "query";
+    }
+
+    @PostMapping
+    public String runQuery(@ModelAttribute("scribe") ScribeDto scribe, Model model) {
+        ScribeDto result = new ScribeDto();
+        try {
+            Long.parseLong(scribe.getCuit().replace("-", ""));
+            result = scribesCollegeService.getScribe(scribe.getCuit());
+        } catch (Exception e) {
+            result.setCuit(scribe.getCuit());
+            log.error("error en el servicio {}", e.getMessage());
+        }
+        model.addAttribute("scribe", result);
+        return "query";
+    }
+
     @GetMapping("/{cuit}")
+    @ResponseBody
     public ScribeDto getScribe(@PathVariable("cuit") Long cuit) {
-        return scribesCollegeService.getScribe(cuit);
+        return scribesCollegeService.getScribe(cuit.toString());
     }
 }
