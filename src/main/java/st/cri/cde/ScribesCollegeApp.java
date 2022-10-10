8d178b76e0bb2bc9d7f4b7e07a0dda2a979cd543
Crist Gimenez <mail@cri.st>
2022-10-10 01:52:50 -0300

    Initial Spring Boot Application

---
+++ src/main/java/st/cri/cde/ScribesCollegeApp.java
@@ -0,0 +1,13 @@
+package st.cri.cde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScribesCollegeApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScribesCollegeApp.class, args);
+	}
+
+}
