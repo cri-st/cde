b01042e98a952222dd4267362e5193b943ec1406
Crist Gimenez <mail@cri.st>
2022-10-10 22:20:05 -0300

    Add Javadoc, apply checktyles, analyse the code with SonarLint, spotbugs and PMD.

--- src/main/java/st/cri/cde/config/AuthToken.java
+++ src/main/java/st/cri/cde/config/AuthToken.java
@@ -4,55 +4,63 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+import javax.crypto.spec.SecretKeySpec;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
-import javax.crypto.spec.SecretKeySpec;
-import java.security.Key;
-import java.util.Date;
-
+/**
+ * The Authentication token.
+ */
 @Slf4j
 @Component
 @AllArgsConstructor
 public class AuthToken {
 
-    private AuthConfig authConfig;
+  private AuthConfig authConfig;
 
-    public String getToken() {
-        String token = createJWT();
-        log.info("Generated token : Bearer {}", token);
-        return "Bearer " + token;
-    }
+  /**
+   * Gets token.
+   *
+   * @return the token
+   */
+  public String getToken() {
+    final String token = createJwt();
+    log.info("Generated token : Bearer {}", token);
+    return "Bearer " + token;
+  }
 
-    private String createJWT() {
-        //The JWT signature algorithm we will be using to sign the token
-        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+  private String createJwt() {
+    //The JWT signature algorithm we will be using to sign the token
+    final SignatureAlgorithm signAlgorithm = SignatureAlgorithm.HS256;
 
-        //We will sign our JWT with our ApiKey secret
-        byte[] apiKeySecretBytes = authConfig.getSecret().getBytes();
-        Key signingKey = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
+    //We will sign our JWT with our ApiKey secret
+    final byte[] apiKeySecretBytes = authConfig.getSecret().getBytes(StandardCharsets.UTF_8);
+    final Key signingKey = new SecretKeySpec(apiKeySecretBytes, signAlgorithm.getJcaName());
 
-        long nowMillis = System.currentTimeMillis();
-        Date now = new Date(nowMillis);
+    final long nowMillis = System.currentTimeMillis();
+    final Date now = new Date(nowMillis);
 
-        //Set Role in claims
-        Claims claims = Jwts.claims();
-        claims.put("role", authConfig.getRole());
+    //Set Role in claims
+    final Claims claims = Jwts.claims();
+    claims.put("role", authConfig.getRole());
 
-        //Let's set the JWT Claims
-        JwtBuilder builder = Jwts.builder()
-                .setClaims(claims)
-                .setSubject(authConfig.getSubject())
-                .setAudience(authConfig.getAudience())
-                .setIssuer(authConfig.getIssuer())
-                .signWith(signingKey, signatureAlgorithm)
-                .setIssuedAt(now);
+    //Let's set the JWT Claims
+    final JwtBuilder builder = Jwts.builder()
+        .setClaims(claims)
+        .setSubject(authConfig.getSubject())
+        .setAudience(authConfig.getAudience())
+        .setIssuer(authConfig.getIssuer())
+        .signWith(signingKey, signAlgorithm)
+        .setIssuedAt(now);
 
-        //Let's add the expiration
-        builder.setExpiration(new Date(nowMillis + 100000));
+    //Let's add the expiration
+    builder.setExpiration(new Date(nowMillis + 100_000));
 
-        //Builds the JWT and serializes it to a compact, URL-safe string
-        return builder.compact();
-    }
+    //Builds the JWT and serializes it to a compact, URL-safe string
+    return builder.compact();
+  }
 }
--- src/main/java/st/cri/cde/dto/ScribeDto.java
+++ src/main/java/st/cri/cde/dto/ScribeDto.java
@@ -4,25 +4,29 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
+/**
+ * The Scribe DTO.
+ */
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class ScribeDto {
-    private String cuit;
-    private String apellido;
-    private String cargo;
-    private String celular;
-    private String dias;
-    private String domicilio;
-    private String email;
-    private String estado;
-    private String fechaDestitucion;
-    private String horario;
-    private String imagen;
-    private int matricula;
-    private boolean mostrarImagen;
-    private String nombre;
-    private int registro;
-    private String sexo;
-    private String telefono;
+public class ScribeDto { //NOPMD - suppressed TooManyFields
+
+  private String cuit;
+  private String apellido;
+  private String cargo;
+  private String celular;
+  private String dias;
+  private String domicilio;
+  private String email;
+  private String estado;
+  private String fechaDestitucion;
+  private String horario;
+  private String imagen;
+  private int matricula;
+  private boolean mostrarImagen;
+  private String nombre;
+  private int registro;
+  private String sexo;
+  private String telefono;
 }
